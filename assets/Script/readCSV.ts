e627ba6d8d7689030778bce9ef2e9bd924317dd8
MrShenLu <2895289240tay@gmail.com>
2019-02-15 14:03:38 +0800

    修复不能读取所有数据的bug 循环对象出错

--- assets/Script/readCSV.ts
+++ assets/Script/readCSV.ts
@@ -4,9 +4,37 @@ const {ccclass, property} = cc._decorator;
 export default class NewClass extends cc.Component {
     @property(cc.Button)
     readBtn:cc.Button=null;
+    @property(cc.Button)
+    deT:cc.Button=null;
 
+    SN:Array<number>=[];
+    resJson:String;
+    delayTime=1;
     onLoad () {
-        
+        let self = this;
+        cc.loader.loadRes("test",function(err,res){
+            if(err){
+                cc.error(err);
+            }
+            else{
+                self.resJson = JSON.stringify(res);
+                // resJson = res
+                let resS = JSON.parse(<string>self.resJson);
+                // let x = resJson.search("r");
+                // console.log(x);
+                // console.log(resJson);
+                // console.log("res length: "+res.length);
+                let RCount=0;
+                for(let u=0;u<self.resJson.length;u++){
+                    if(self.resJson[u]=="r"){
+                        self.SN.push(u);
+                        RCount++;
+                        // console.log("SN Length: "+SN.length+"  r location "+u);
+                    }
+                }
+                console.log("Rcount "+RCount);
+            }
+        });
     }
 
     start () {
@@ -16,52 +44,29 @@ export default class NewClass extends cc.Component {
     // update (dt) {}
 
     read(){
+        let self = this
         let contents:Array<String>=[];
-        let resJson:String;
-        let SN:Array<number>=[];
         let Content:Array<String>=[];
-        cc.loader.loadRes("test",function(err,res){
-            if(err){
-                cc.error(err);
+        let y;
+        for(let i=0;i<self.SN.length;i++){
+            if(i==0){
+                y = self.resJson.substring(0,self.SN[i]);
+                y = y.substring(1,y.length-1);
+                // console.log("y : "+y);
+                Content.push(y);
             }
             else{
-                resJson = JSON.stringify(res);
-                // resJson = res
-                let resS = JSON.parse(<string>resJson);
-                // let x = resJson.search("r");
-                // console.log(x);
-                console.log(resJson);
-                for(let u=0;u<res.length;u++){
-                    if(resJson[u]=="r"){
-                        SN.push(u);
-                    }
-                }
+                y = self.resJson.substring(self.SN[i-1],self.SN[i]);
+                y = y.substring(y.search("n"));
+                y = y.substring(1,y.length-1);
+                // console.log("y : "+y);
+                Content.push(y);
             }
-        });
-
-        this.scheduleOnce(function(){
-            let y;
-            for(let i=0;i<SN.length;i++){
-                if(i==0){
-                    y = resJson.substring(0,SN[i]);
-                    y = y.substring(1,y.length-1);
-                    console.log("y : "+y);
-                    Content.push(y);
-                }
-                else{
-                    y = resJson.substring(SN[i-1],SN[i]);
-                    y = y.substring(y.search("n"));
-                    y = y.substring(1,y.length-1);
-                    console.log("y : "+y);
-                    Content.push(y);
-                }
-            }
-            console.log(Content);
-        },2);
-        
-        // let x = resJson.split("\r\n");
-        // let y = resJson.substr(1,5);
-        // console.log(x);
-        // console.log(y);
+        }
+        console.log(Content);
+        console.log(this.delayTime);
+    }
+    ChangeTime(){
+        this.delayTime-=0.1;   
     }
 }
