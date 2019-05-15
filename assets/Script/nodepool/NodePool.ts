d77f376d019ac4b87f0de5c7c3b6e54cffe30425
MrShenLu <2895289240tay@gmail.com>
2019-05-15 17:44:29 +0800

    。

---
+++ assets/Script/nodepool/NodePool.ts
@@ -0,0 +1,18 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NodePool extends cc.Component {
+
+    @property(cc.Label)
+    
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
