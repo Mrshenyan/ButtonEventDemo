088671ab724210a53ea60cf911f4931267aa359d
MrShenLu <2895289240tay@gmail.com>
2019-06-13 16:42:59 +0800

    对文件读取的本分进行一定的修改。

--- assets/Script/others/other/readCSV.ts
+++ assets/Script/others/other/readCSV.ts
@@ -16,14 +16,20 @@ export default class NewClass extends cc.Component {
         let self = this;
         let prefab = cc.instantiate(this.BtnNodes);
         this.node.addChild(prefab);
+        let xxxx = "000011";
+        let yyyy = parseInt(xxxx);
+        console.log(yyyy);
         //以下是CVS文件读取并转换为数组存储。
-        cc.loader.loadRes("jsonf",function(err,res){
+        cc.loader.loadRes("test2",function(err,res){
             if(err){
                 cc.error(err);
             }
             else{
-                console.log(res);
-                self.resJson = JSON.stringify(res);
+                console.log(res.text);
+                // let x = parseFloat(res.json[0].features.DE);
+                // let y=0;
+                // let z=x+y;
+                self.resJson = JSON.stringify(res.text);
                 let resS = JSON.parse(<string>self.resJson);
                 let RCount=0;
                 for(let u=0;u<self.resJson.length;u++){
@@ -47,6 +53,7 @@ export default class NewClass extends cc.Component {
         let self = this
         let contents:Array<String>=[];
         let Content:Array<String>=[];
+        let xxxx = [];
         let y;
         for(let i=0;i<self.SN.length;i++){
             if(i==0){
@@ -64,8 +71,31 @@ export default class NewClass extends cc.Component {
                
             }
         }
+        xxxx = Serialization();
         console.log(Content);
         console.log(this.delayTime);
+
+
+        /**
+         * 
+         */
+        function Serialization():Array<[]>{
+            let dotpos=[];
+            let matchStr:string = ",";
+            for(let i=0;i<Content.length;i++){
+                dotpos[i]=[];
+                let tempCon = Content[i];
+                for(let j=0;j<Content[i].length-1;j++){
+                    if(tempCon.substring(j,j+1)==matchStr){
+                        dotpos[i].push(j);
+                    }
+                }
+            }
+            let str = Content[1].substring(dotpos[1][2],dotpos[1][3]);
+            console.log("str: "+str);
+            console.log(dotpos);
+            return dotpos;
+        }
     }
     ChangeTime(){
         this.delayTime-=0.1;   
