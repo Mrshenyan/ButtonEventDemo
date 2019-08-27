ac8a7a796c38eec6086f87ca4aa40173f0f90e47
MrShenLu <2895289240tay@gmail.com>
2019-08-27 15:50:26 +0800

    调整棋子的生成函数，相消函数初始

--- assets/Script/SixTest/Sixtext.ts
+++ assets/Script/SixTest/Sixtext.ts
@@ -34,8 +34,7 @@ export default class Sixtext extends cc.Component {
         this.generaterCell();
         let self = this;
         this.RotateNode.on(cc.Node.EventType.TOUCH_START,()=>{
-            let shadow = cc.instantiate(self.Shadow);
-
+            // let shadow = cc.instantiate(self.Shadow);
         })
         this.RotateNode.on(cc.Node.EventType.TOUCH_MOVE,this.Move,this);
         this.RotateNode.on(cc.Node.EventType.TOUCH_END,this.MoveEnd,this);
@@ -73,8 +72,12 @@ export default class Sixtext extends cc.Component {
                     }
                     this.node.addChild(newCell);
                     j++;
-                    let attr={tag:{n,i,count}};
+                    let row = i;
+                    let sn = count;
+                    let attr={tag:{n,row,sn}};
+                    let attr2={isFilled:false};
                     newCell.attr(attr);
+                    newCell.attr(attr2);
                     this.allCellPos[n][count] = new Array();
                     this.allCellPos[n][count].push(newCell);
                     this.allCellPos[n][count].push(cellPos);
@@ -82,7 +85,6 @@ export default class Sixtext extends cc.Component {
                     this.allCellPos[n][count]["SN"].push(n);
                     this.allCellPos[n][count]["SN"].push(i);
                     this.allCellPos[n][count]["SN"].push(count);
-                    this.allCellPos[n][count].isFilled = false;
                     count++;
                 }
             }
@@ -117,19 +119,19 @@ export default class Sixtext extends cc.Component {
                         }
                         this.node.addChild(newCell);
                         j++;
-                        let attr={tag:{n,i,count}};
+                        let row = i;//中间向两边
+                        let sn = count;//自左向右，从0开始
+                        let attr={tag:{n,row,sn}};
+                        let attr2={isFilled:false};
                         newCell.attr(attr);
+                        newCell.attr(attr2);
                         this.allCellPos[n][count] = new Array();
                         this.allCellPos[n][count].push(newCell);
                         this.allCellPos[n][count].push(cellPos);
                         this.allCellPos[n][count]["SN"] = new Array();
                         this.allCellPos[n][count]["SN"].push(n);
                         this.allCellPos[n][count]["SN"].push(i);
-                        this.allCellPos[n][count]["SN"].push(count)
-                        this.allCellPos[n][count].isFilled = false;
-                        // let isfill = new addProtot(this.allCellPos);
-                        // addProtot.prototype.isFilled = null;
-                        // isfill.isFilled = false;
+                        this.allCellPos[n][count]["SN"].push(count);
                         count++;
                     }
                 }
@@ -137,10 +139,10 @@ export default class Sixtext extends cc.Component {
             
 
         }
-        function addProtot(array){
-            this.allCellPos
-        }
-        console.log(this.allCellPos);
+        // function addProtot(array){
+        //     this.allCellPos
+        // }
+        // console.log(this.allCellPos);
     }
 
 
@@ -181,28 +183,33 @@ export default class Sixtext extends cc.Component {
     CellClickCallBack(event,customEventTarget){
         // let currentTarget =
         let tag = event.currentTarget.tag;
-        console.log(tag);
+        // console.log(tag);
         //这里的this是调用的时候传入的响应节点
-        this.FindCell(tag);
+        let nodes = this.FindCell(tag);//tag是点击的游戏格的位置属性和第几个
+        // console.log(nodes);
     }
 
     
     /**
-     * 一键消除，获取点击的单元格的周围游戏格。
+     * 获取点击的单元格的周围游戏格。
      * @param tag 被点击的单元格信息
-     * @param toolKind 使用的道具类型，1：一键消除，2：小导弹，3：强制合成
+     * @param toolKind 使用的道具类型，1.0：一键消除，2.1,2.2,2.3：小导弹，3.0：强制合成
+     * 注：小导弹有三种：横向，左斜向，右斜向，分别对应2.1，2.2，2.3
+     * 这个查找函数用于返回使用道具之后的需要消除的游戏格
      */
-    FindCell(tag,toolKind?){
+    FindCell(tag,toolKind?):cc.Node[]{
         let lineNum = tag.n;
-        let RowNum = tag.i;
-        let CellSN = tag.count;
-        console.log(lineNum);
-        console.log(RowNum);
+        let RowNum = tag.row;
+        let CellSN = tag.sn;
+        // console.log(lineNum);
+        // console.log(RowNum);
         let x =lineNum/2 - parseInt((lineNum/2).toString());
         let RoundSixCellSN = new Array(6);
         for(let o=0;o<RoundSixCellSN.length;o++){
             RoundSixCellSN[o] = new Array();
         }
+        /**使用技能返回的游戏格 */
+        let ToolKindNodes=new Array<cc.Node>();
         RoundSixCellSN[0].push(lineNum-1);
         RoundSixCellSN[0].push(CellSN-1);
         RoundSixCellSN[1].push(lineNum-1);
@@ -215,7 +222,7 @@ export default class Sixtext extends cc.Component {
         RoundSixCellSN[4].push(CellSN);
         RoundSixCellSN[5].push(lineNum+1);
         RoundSixCellSN[5].push(CellSN+1);
-        console.log(RoundSixCellSN);
+        // console.log(RoundSixCellSN);
         let RoundSixCellNodes=new Array();
         
         for(let i=0;i<RoundSixCellSN.length;i++){
@@ -230,7 +237,43 @@ export default class Sixtext extends cc.Component {
                 continue;
             }
         }
-        console.log(RoundSixCellNodes);
+        RoundSixCellNodes[RoundSixCellNodes.length] = this.allCellPos[lineNum][CellSN]
+        // console.log(RoundSixCellNodes);
+        switch(toolKind){
+            case 1:{
+                ToolKindNodes = RoundSixCellNodes;
+                break;
+            }
+            case 2.1:{
+                ToolKindNodes[0] = RoundSixCellNodes[2];
+                ToolKindNodes[1] = RoundSixCellNodes[3];
+                ToolKindNodes[2] = RoundSixCellNodes[6];
+                break;
+            }
+            case 2.2:{
+                ToolKindNodes[0] = RoundSixCellNodes[0];
+                ToolKindNodes[1] = RoundSixCellNodes[5];
+                ToolKindNodes[2] = RoundSixCellNodes[6];
+                break;
+            }
+            case 2.3:{
+                ToolKindNodes[0] = RoundSixCellNodes[1];
+                ToolKindNodes[1] = RoundSixCellNodes[4];
+                ToolKindNodes[2] = RoundSixCellNodes[6];
+                break;
+            }
+            case 3:{
+                //强制合成要另外处理，等第二次选择结束
+                ToolKindNodes[0] = RoundSixCellNodes[2];
+                ToolKindNodes[1] = RoundSixCellNodes[3];
+                ToolKindNodes[2] = RoundSixCellNodes[6];
+                break;
+            }
+            default:{
+                return RoundSixCellNodes;
+            }
+        }
+        return ToolKindNodes;
     }
 
     /**
@@ -243,6 +286,8 @@ export default class Sixtext extends cc.Component {
         let count=0;
         let rotate=0;
         let genedNumGrid:Array<cc.Node>=new Array();
+        let attr={tag:null};
+        let num={theNum:0};
         if(!count){
             count = parseInt((Math.random() * 2).toString()) + 1, rotate = parseInt((Math.random() * 6).toString()) * 60;
         }
@@ -254,11 +299,12 @@ export default class Sixtext extends cc.Component {
                 console.log("gameover");
                 break;
             }
-            case 1:{//generate one number grid
-                console.log("generate one number grid");
+            case 1:{//generate one number grid，这里只生成一个棋子
+                console.log("generate one number grid only");
+                geneFun()
                 break;
             }
-            default:{//generate two number grid
+            default:{//generate two number grid,要说明的是这里生成1个或者两个棋子
                 console.log("generate two number grid");
                 geneFun();
                 break;
@@ -270,22 +316,34 @@ export default class Sixtext extends cc.Component {
             if(!count||count==1){
                 let id = parseInt((Math.random()*geneID.length).toString());
                 genedNumGrid[0] = cc.instantiate(self.Chesses[geneID[id]]);
+                genedNumGrid[0].attr(attr);
+                num.theNum = geneID[id]+1;
+                genedNumGrid[0].attr(num);
+                console.log("生成的是："+num.theNum);
                 geneID.splice(id,1)
                 genePos[0]=genedNumGrid[0].position.rotate(rotate);
                 // let 
             }
             else{
-                console.log(geneID);
+                // console.log(geneID);
                 let id = parseInt((Math.random()*geneID.length).toString());
                 genedNumGrid[0] = cc.instantiate(self.Chesses[geneID[id]]);
+                genedNumGrid[0].attr(attr);
+                num.theNum = geneID[id]+1;
+                genedNumGrid[0].attr(num);
+                console.log("生成的是："+num.theNum);
                 geneID.splice(id,1)
-                console.log("id: "+id);
-                console.log(geneID);
+                // console.log("id: "+id);
+                // console.log(geneID);
                 id = parseInt((Math.random()*geneID.length).toString());
                 genedNumGrid[1] = cc.instantiate(self.Chesses[geneID[id]]);
+                genedNumGrid[1].attr(attr);
+                num.theNum = geneID[id]+1;
+                console.log("生成的是："+num.theNum);
+                genedNumGrid[1].attr(num);
                 geneID.splice(id,1)
-                console.log("id: "+id);
-                console.log(geneID);
+                // console.log("id: "+id);
+                // console.log(geneID);
                 genedNumGrid[0].x=99;
                 genedNumGrid[1].x=-99
                 genePos[0]=genedNumGrid[0].position.rotate(rotate*Math.PI/180);
@@ -312,6 +370,7 @@ export default class Sixtext extends cc.Component {
         let touchpos = event.touch.getDelta();
         this.RotateNode.x += touchpos.x;
         this.RotateNode.y += touchpos.y;
+        self.FilledPos=[];
         RotateLoop:
         for(let n=0;n<this.RotateNode.childrenCount;n++){
             allCellLoop:
@@ -332,14 +391,18 @@ export default class Sixtext extends cc.Component {
         function alignPos(pos1,pos2,node,n,count):boolean{
             let v = new cc.Vec2(pos1.x-pos2.x,pos1.y-pos2.y)
             let det = cc.v2().sub(v).mag();;
-            if(det<node.width/2){
+            if(det<(node.width/2-10)){
                 node.getChildByName("shadow").active = true;
-                self.FilledPos[n] = node
+                self.FilledPos.push(node);
+                if(node.isFilled&&count==2){
+                    self.FilledPos=[];
+                }
                 return true;
             }
             else{
                 if((n!=1)||(count<2)){
                     node.getChildByName("shadow").active = false;
+                    self.FilledPos=[];
                 }
                 return false;
             }
@@ -351,22 +414,180 @@ export default class Sixtext extends cc.Component {
      * @param event 
      */
     MoveEnd(event){
+        let self = this;
+        let fpos=new Array();
         if(!this.FilledGrid){
+            this.RotateNode.setPosition(this.ariPos);
+        }
+        if(this.FilledPos.length<1){
             this.RotateNode.setPosition(this.ariPos);
         }
         for(let i=0;i<this.FilledPos.length;i++){
             if(this.FilledPos[i]==null||this.FilledPos[i]==undefined){
                 this.RotateNode.setPosition(this.ariPos);
             }
+            else if(this.FilledPos.length!=this.RotateNode.childrenCount){
+                this.FilledPos[i].children[0].active=false;
+                this.RotateNode.setPosition(this.ariPos);
+            }
             else{
+                this.FilledPos[i].isFilled=true;
                 this.RotateNode.children[i].setPosition(0,0); 
-                this.RotateNode.children[i].parent = this.FilledPos[i]
+                this.RotateNode.children[i].tag =  this.FilledPos[i].tag;
+                this.RotateNode.children[i].parent = this.FilledPos[i];
+                // self.Eliminate(this.FilledPos[i].children[1].tag);
+                fpos[fpos.length]=this.FilledPos[i];
                 this.FilledPos.shift();
                 i=-1;
                 this.FilledGridCount-=1;
                 this.RotateNode.setPosition(this.ariPos);
             }
         }
+        for(let i=0;i<fpos.length;i++){
+            self.Eliminate(fpos[i].children[1].tag);
+        }
         this.FilledGrid = false;
+        this.FilledPos=[]
+    }
+
+    /**
+     * @param tag 数字在游戏区域的位置
+     * 消除函数
+     */
+    Eliminate(tag:{n,row,sn}){
+        // console.log(tag);
+        let ArroundNodes = this.FindCell(tag);
+        // console.log(ArroundNodes);//最后一个是自身;
+        // console.log(this.allCellPos);
+        let leftNode=this.allCellPos[tag.n][tag.sn-1],rightNode=this.allCellPos[tag.n][tag.sn+1];
+        let downLeftNode,downRightNode;
+        let theNode = this.allCellPos[tag.n][tag.sn];
+        if(tag.n==9){
+            downLeftNode=undefined;
+            downRightNode=undefined;
+        }else{
+            downLeftNode = this.allCellPos[tag.n+1][tag.sn]
+            downRightNode = this.allCellPos[tag.n+1][tag.sn+1];
+        }
+        let leftIsFilled,rightIsFilled,downLeftIsFilled,downRightIsFilled;//左，右，左下，右下,0表示为空，可以填入数字
+        if(leftNode==undefined){
+            leftIsFilled=0b1111;
+        }
+        else{
+            if(!leftNode[0].isFilled){
+                leftIsFilled=0b0000;
+            }
+            else{
+                leftIsFilled=0b0010;
+            }
+        }
+        if(rightNode==undefined){
+            rightIsFilled=0b1111;
+        }
+        else{
+            if(!rightNode[0].isFilled){
+                rightIsFilled=0b0000;
+            }
+            else{
+                rightIsFilled=0b0001;
+            }
+        }
+        if(downLeftNode==undefined){
+            downLeftIsFilled=0b1111;
+        }
+        else{
+            if(!downLeftNode[0].isFilled){
+                downLeftIsFilled=0b0000;
+            }
+            else{
+                downLeftIsFilled=0b1000;
+            }
+        }
+        if(downRightNode==undefined){
+            downRightIsFilled=0b1111;
+        }
+        else{
+            if(!downRightNode[0].isFilled){
+                downRightIsFilled=0b0000;
+            }
+            else{
+                downRightIsFilled=0b0100;
+            }
+        }
+        
+        // console.log("leftIsFilled: "+leftIsFilled)
+        // console.log("rightIsFilled: "+rightIsFilled)
+        // console.log("downFeftIsFilled: "+downLeftIsFilled)
+        // console.log("downRightIsFilled: "+downRightIsFilled)
+        let allFourCellFill:number=0b1111;
+        let LPulsR = leftIsFilled+rightIsFilled;
+        let x = LPulsR|0b0000//按位于之后结果为0表示左右都有空，即不需要消除
+        if(!x){
+            console.log("X: "+x);
+            console.log("不需要消除")
+        }else{
+            switch(x){
+                case 0b0010:{
+                    console.log("左");
+                    if(theNode[0].children[1].theNum==leftNode[0].children[1].theNum){
+                        console.log("相等，可以相消");
+                        if(!downLeftIsFilled){
+                            console.log("左下是空的，可以填入");
+                        }else{
+                            console.log("左下不是空的，直接消除");
+                        }
+                    }else{
+                        console.log("不相等，不可以相消");
+                    }
+                    break;
+                }
+                case 0b0001:{
+                    console.log("右");
+                    if(theNode[0].children[1].theNum==rightNode[0].children[1].theNum){
+                        console.log("相等，可以相消");
+                        if(!downRightIsFilled){
+                            console.log("左下是空的，可以填入");
+                        }else{
+                            console.log("左下不是空的，直接消除");
+                        }
+                    }else{
+                        console.log("不相等，不可以相消");
+                    }
+                    break;
+                }
+                case 0b0011:{
+                    console.log("左右");
+                    break;
+                }
+                default:{
+                    if(leftIsFilled&&leftIsFilled<5){
+                        console.log("最右端，左有数字");
+                        if(theNode[0].children[1].theNum==leftNode[0].children[1].theNum){
+                            console.log("相等，可以相消");
+                            if(!downLeftIsFilled){
+                                console.log("左下是空的，可以填入");
+                            }else{
+                                console.log("左下不是空的，直接消除");
+                            }
+                        }else{
+                            console.log("不相等，不可以相消");
+                        }
+                    }
+                    else if(rightIsFilled&&rightIsFilled<5){
+                        console.log("最左端，右有数字");
+                        if(theNode[0].children[1].theNum==rightNode[0].children[1].theNum){
+                            console.log("相等，可以相消");
+                            if(!downRightIsFilled){
+                                console.log("左下是空的，可以填入");
+                            }else{
+                                console.log("左下不是空的，直接消除");
+                            }
+                        }else{
+                            console.log("不相等，不可以相消");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
