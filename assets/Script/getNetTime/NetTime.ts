7046e954ad39d5640703d8202e2b4e2425ce1d35
MrShenLu <2895289240tay@gmail.com>
2019-09-17 17:41:40 +0800

    获取网络时间

--- assets/Script/getNetTime/NetTime.ts
+++ assets/Script/getNetTime/NetTime.ts
@@ -1,30 +1,38 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+import HttpManager from "../HttpManager";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class getNetTime extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
-
+    _url:string="http://api.k780.com:88/?app=life.time&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json"
     @property
-    text: string = 'hello';
+    set url(urlStr){
+        this._url = urlStr;
+    }
+    get url(){
+        return this._url;
+    }
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
     start () {
-
+        let url = this._url;
+        let timeXML =new XMLHttpRequest();
+        timeXML.onreadystatechange = function(){
+            if(timeXML.readyState == 4 && (timeXML.status>= 200 && timeXML.status<400)){
+                let res = timeXML.responseText;
+                console.log(JSON.parse(res));
+            }
+        }
+        timeXML.open("GET",url,true);
+        timeXML.send();
+        // let str = timeXML.
+        // console.log(str);
     }
 
     // update (dt) {}
