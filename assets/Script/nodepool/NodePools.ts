fd5501b5ee04ddfc1745af46562fc350d7da0988
MrShenLu <2895289240tay@gmail.com>
2019-09-23 17:54:23 +0800

    修改代码

--- assets/Script/nodepool/NodePools.ts
+++ assets/Script/nodepool/NodePools.ts
@@ -3,16 +3,36 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class NodePools extends cc.Component {
 
-    // @property(cc.Label)
-    // testt
+    
+    
+    NodePool:cc.NodePool=null;
+    @property(cc.Prefab)
+    nodePre:cc.Prefab=null;
+    @property(cc.Node)
+    nodeParent:cc.Node=null;
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
+    onLoad () {
+        this.NodePool = new cc.NodePool();
+        let poolcapacity = 10;
+        for(let i=0;i<poolcapacity;i++){
+            let node = cc.instantiate(this.nodePre);
+            this.NodePool.put(node);
+        }
+    }
 
     start () {
 
     }
 
     // update (dt) {}
+
+    addObj(){
+        let obj = null;
+        if(this.NodePool.size()>0){
+            obj = this.NodePool.get();
+        }else{
+            obj = cc.instantiate(this.nodePre);
+        }
+        this.nodeParent.addChild(obj);
+    }
 }
