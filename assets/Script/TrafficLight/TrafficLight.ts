26610a306516367a687e05e3fa49b26da2b442a8
Mrshenyan <13085028720@163.com>
2019-10-14 18:48:35 +0800

    扎金花

---
+++ assets/Script/TrafficLight/TrafficLight.ts
@@ -0,0 +1,22 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class TrafficLight extends cc.Component {
+
+    @property(cc.Sprite)
+    redSp:cc.Sprite=null;
+    @property(cc.Sprite)
+    greenSp:cc.Sprite=null;
+    @property(cc.Sprite)
+    yellowSp:cc.Sprite=null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
