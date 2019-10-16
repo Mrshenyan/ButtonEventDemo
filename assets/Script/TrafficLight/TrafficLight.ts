df96139b9d609139a9bc120e8c9a08a670192be4
Mrshenyan <13085028720@163.com>
2019-10-16 09:17:40 +0800

    状态锁（初始）

--- assets/Script/TrafficLight/TrafficLight.ts
+++ assets/Script/TrafficLight/TrafficLight.ts
@@ -3,12 +3,13 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class TrafficLight extends cc.Component {
 
-    @property(cc.Sprite)
-    redSp:cc.Sprite=null;
+   
     @property(cc.Sprite)
     greenSp:cc.Sprite=null;
     @property(cc.Sprite)
-    yellowSp:cc.Sprite=null;
+    yellowSp:cc.Sprite=null; 
+    @property(cc.Sprite)
+    redSp:cc.Sprite=null;
 
     // LIFE-CYCLE CALLBACKS:
 
